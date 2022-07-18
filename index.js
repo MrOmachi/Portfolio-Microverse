d20a6d9b67c231dc18313ff7b081ff2451b94e98
Omachi John <omachi.john@.com>
2022-07-19 00:25:57 +0100

    Add styling to list menu links, Make corrections to linters errors

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-let closeX = document.getElementById('closeX');
-let menu = document.getElementById('menu');
-let hamburger = document.getElementById('hamburger');
-let menu1 = document.getElementById("menu-link1");
-let menu2 = document.getElementById("menu-link2");
-let menu3 = document.getElementById("menu-link3");
+const closeX = document.getElementById('closeX');
+const menu = document.getElementById('menu');
+const hamburger = document.getElementById('hamburger');
 
 [closeX, menu, hamburger].forEach((elements) => {
-  elements.addEventListener('click', function () {
-    menu.classList.toggle("hidden");
-    closeX.classList.toggle("hidden");
+  elements.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+    closeX.classList.toggle('hidden');
     hamburger.classList.toggle('hidden');
   });
-}) 
+});
