dc6cabb38910600b99e7a3f3ebb9c82d7a36eea7
Omachi John <omachi.john@.com>
2022-07-18 21:17:04 +0100

    Add toggle effect to mobile menu icons

---
+++ index.js
@@ -0,0 +1,5 @@
+let closeX = document.getElementById('closeX');
+let menu = document.getElementById('menu');
+let hamburger = document.getElementById('hamburger');
+
+hamburger.addEventListener('click', function () {});
