f8e117104debd0111f0af1329480ef9a14284114
inakiesk <inakieskruiz@gmail.com>
2022-07-18 15:56:37 -0500

    Add hide/display function to menu

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 let closeX = document.getElementById('closeX');
 let menu = document.getElementById('menu');
 let hamburger = document.getElementById('hamburger');
+let menu1 = document.getElementById("menu-link1");
+let menu2 = document.getElementById("menu-link2");
+let menu3 = document.getElementById("menu-link3");
 
-hamburger.addEventListener('click', function () {});
+[closeX, menu, hamburger].forEach((elements) => {
+  elements.addEventListener('click', function () {
+    menu.classList.toggle("hidden");
+    closeX.classList.toggle("hidden");
+    hamburger.classList.toggle('hidden');
+  });
+}) 
