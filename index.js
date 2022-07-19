0c5d9f63fd2e84ed1b6614dd1fbbadab5c16d88a
Omachi John <omachi.john@.com>
2022-07-19 10:27:01 +0100

    Hide logo on click of hamburger icon to fit in figma design

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const closeX = document.getElementById('closeX');
 const menu = document.getElementById('menu');
 const hamburger = document.getElementById('hamburger');
+const logoMobile = document.getElementById('logoMobile');
 
 [closeX, menu, hamburger].forEach((elements) => {
   elements.addEventListener('click', () => {
     menu.classList.toggle('hidden');
     closeX.classList.toggle('hidden');
     hamburger.classList.toggle('hidden');
+    logoMobile.classList.toggle('noVisibility');
   });
 });
