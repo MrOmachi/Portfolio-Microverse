abce95c1dc7d30e33ab5c11bfaeaa4a250b582b4
Omachi John <omachi.john@.com>
2022-07-20 15:38:23 +0100

    Create modal section for my portfolio

--- index.js
+++ index.js
@@ -2,6 +2,65 @@ const closeX = document.getElementById('closeX');
 const menu = document.getElementById('menu');
 const hamburger = document.getElementById('hamburger');
 const logoMobile = document.getElementById('logoMobile');
+const btnId1 = document.getElementById('btnId1');
+const btnId2 = document.getElementById('btnId2');
+const btnId3 = document.getElementById('btnId3');
+const btnId4 = document.getElementById('btnId4');
+const workArr = [
+  {
+    id: '0',
+    title: 'Multy Post - Stories',
+    image: 'images/SnapshootPortfolio.png',
+    paragraph:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
+    ulList1: 'html',
+    ulList2: 'Ruby on rail',
+    ulList3: 'css',
+    btnLive: 'https://mromachi.github.io/Portfolio-Microverse/',
+    btnSource: 'https://github.com/MrOmachi/Portfolio-Microverse ',
+  },
+
+  {
+    id: '1',
+    title: 'Second Multy Post - Stories ',
+    image: 'images/SnapshootPortfolio.png',
+    paragraph:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
+    ulList1: 'html',
+    ulList2: 'Ruby on rail',
+    ulList3: 'css',
+    btnLive: 'https://mromachi.github.io/Portfolio-Microverse/',
+    btnSource: 'https://github.com/MrOmachi/Portfolio-Microverse ',
+  },
+
+  {
+    id: '2',
+    title: 'Third Multy Post - Stories',
+    image: 'images/SnapshootPortfolio.png',
+    paragraph:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
+    ulList1: 'html',
+    ulList2: 'Ruby on rail',
+    ulList3: 'css',
+    btnLive: 'https://mromachi.github.io/Portfolio-Microverse/',
+    btnSource: 'https://github.com/MrOmachi/Portfolio-Microverse ',
+  },
+
+  {
+    id: '3',
+    title: 'Fourth Multy Post - Stories',
+    image: 'images/SnapshootPortfolio.png',
+    paragraph:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
+    ulList1: 'html',
+    ulList2: 'Ruby on rail',
+    ulList3: 'css',
+    btnLive: 'https://mromachi.github.io/Portfolio-Microverse/',
+    btnSource: 'https://github.com/MrOmachi/Portfolio-Microverse ',
+  },
+];
+
+let i = 0;
 
 [closeX, menu, hamburger].forEach((elements) => {
   elements.addEventListener('click', () => {
@@ -10,4 +69,44 @@ const logoMobile = document.getElementById('logoMobile');
     hamburger.classList.toggle('hidden');
     logoMobile.classList.toggle('noVisibility');
   });
+});
+
+function display() {
+  const popScreen = document.querySelector('body');
+  const modalSection = document.createElement('section');
+  const sectionDivContainer = document.createElement('div');
+  const sectionDiv = document.createElement('div');
+  const sectionHeader = document.createElement('header');
+  const divTitle = document.createElement('h1');
+  const divClosImg = document.createElement('img');
+  const divImgContainer = document.createElement('div');
+  const divImg = document.createElement('img');
+  const divParagraph = document.createElement('p');
+  const ulLi1st = document.createElement('ul');
+  const ulLi1 = document.createElement('li');
+  const ulLi2 = document.createElement('li');
+  const ulLi3 = document.createElement('li');
+  const divButton = document.createElement('div');
+  const popBtn1 = document.createElement('button');
+  const popBtn2 = document.createElement('button');
+}
+
+btnId1.addEventListener('click', () => {
+  i = 0;
+  display();
+});
+
+btnId2.addEventListener('click', () => {
+  i = 1;
+  display();
+});
+
+btnId3.addEventListener('click', () => {
+  i = 2;
+  display();
+});
+
+btnId4.addEventListener('click', () => {
+  i = 3;
+  display();
 });
