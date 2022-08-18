a690c3373598d80684202a09db4eaf3e34cc7ad4
Omachi John <omachi.john@.com>
2022-08-18 01:53:22 +0100

    Add first project

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ const myForm = document.getElementById('myForm');
 const workArr = [
   {
     id: '0',
-    title: 'Multy Post - Stories',
-    image: 'images/SnapshootPortfolio.png',
+    title: 'Awesome Books Library',
+    image: 'images/desktop.png',
     paragraph:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
+      'This educational project involves creating a straightforward website that shows a list of books and lets you add and remove books from that list. The intention is to use modules and learn ES6 syntax to make it more orderly. built with JavaScript, CSS and HTML.',
     ulList1: 'html',
     ulList2: 'Ruby on rail',
     ulList3: 'css',
-    btnLive: 'https://mromachi.github.io/Portfolio-Microverse/',
-    btnSource: 'https://github.com/MrOmachi/Portfolio-Microverse ',
+    btnLive: 'https://mromachi.github.io/awsomeBooksInModules/',
+    btnSource: 'https://github.com/MrOmachi/awsomeBooksInModules.git',
   },
 
   {
